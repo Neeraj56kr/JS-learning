0638239aaaf184fc3b294833c6da23e57ee56b35
Neeraj Kumar <148734493+Neeraj56kr@users.noreply.github.com>
2025-12-12 14:58:27 +0530

    Asynchronous

---
+++ New folder/js/script.js
@@ -0,0 +1,31 @@
+// function ch() {
+//     console.log("hello");
+// }
+// function fun(callback) {
+//     callback();             // non blocking way of executing function
+//     console.log("fun");
+// }
+// fun(ch)
+
+// setTimeout(() => {
+//     console.log("timing function with callback function");
+// })
+// console.log("Normal line");
+
+const p = new Promise((resolve, reject) => {
+    let a = true;
+    if (a) {
+        return resolve("Promise resolved");
+    } else {
+        return reject("Promise rejected");
+    }
+});
+p.then((a) => {
+    console.log(a);
+})
+.then(() => {
+    console.log("new line");
+})
+.catch((a) => {
+    console.log(a);
+});
